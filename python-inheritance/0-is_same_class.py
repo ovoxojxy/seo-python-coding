e6225293357e654c5dfa6c734c5476d81bfed31e
ovoxojxy <jjarod517@gmail.com>
2023-06-08 16:23:10 +0000

    inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''This mod conatins a function that returns true if the object is 
+'''This mod conatins a function that returns true if the object is
 exactly an instance of the specified class; otherwise False'''
 
 
