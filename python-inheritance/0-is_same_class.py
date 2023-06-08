f11f559ece2027d8eeafab96f3078157762c0e31
ovoxojxy <jjarod517@gmail.com>
2023-06-08 16:22:20 +0000

    inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''This mod conatins a function that returns true if the object is exactly an instance of the specified class; otherwise False'''
+'''This mod conatins a function that returns true if the object is 
+exactly an instance of the specified class; otherwise False'''
 
 
 def is_same_class(obj, a_class):
-	'''this is the function referenced in the above doc'''
-	return(type(obj) == a_class)
+    '''this is the function referenced in the above doc'''
+    return(type(obj) == a_class)
