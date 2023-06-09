a0d6bd8bb4649b70d885c5d00942aabfe7683451
ovoxojxy <jjarod517@gmail.com>
2023-06-09 14:16:11 +0000

    inheritance

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''This mod creates a function that returns true or false'''
+'''This mod creates a function that returns true or false 
+depending on whether the obj is from the same instance as\
+the object in the functions argument or not'''
 def is_kind_of_class(obj, a_class):
 '''This function returns true if an object is the same instanc
 as the class argument'''
