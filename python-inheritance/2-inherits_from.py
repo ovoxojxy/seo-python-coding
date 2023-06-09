7d60d947d976ef8b20d6c6e60db14a0329235128
ovoxojxy <jjarod517@gmail.com>
2023-06-09 14:32:21 +0000

    inheritance

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This mod is to write a function that returns True
+if the object is an instance of a class that inherited
+f_r_o_m the specified class; otherwise False'''
+
+
+def inherits_from(obj, a_class):
+    '''returns True if the object is an instance of a
+    class that inherited f_r_o_m the specified class;
+    otherwise False'''
+    return isinstance(obj, a_class) or issubclass(obj,a_class)
