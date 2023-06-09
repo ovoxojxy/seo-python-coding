a743ed8590b344b542b6550351a19274d5dd96de
ovoxojxy <jjarod517@gmail.com>
2023-06-09 14:42:44 +0000

    inheritance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     '''returns True if the object is an instance of a
     class that inherited f_r_o_m the specified class;
     otherwise False'''
-    return  any(isinstance(cls, a_class) for cls in type(obj).__mro__)
+    return any(isinstance(cls, a_class) for cls in type(obj).__mro__)
