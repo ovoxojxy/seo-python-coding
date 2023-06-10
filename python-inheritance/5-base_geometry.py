47bb2c9291393f932040e8a616c5e1579924b0a3
ovoxojxy <jjarod517@gmail.com>
2023-06-10 03:52:58 +0000

    docs

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,4 +6,11 @@ area(self)'''
 
 class BaseGeometry:
     '''Class with private width and height instantiations'''
-    pass
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+	    raise TypeError(f'{name} must be an integer')
+	if values<= 0;
+	    raise ValueError(f'{name} must be greater than 0')
