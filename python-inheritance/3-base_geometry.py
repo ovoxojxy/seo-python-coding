293af76effcd5aeb42df2a92979fce3213756636
ovoxojxy <jjarod517@gmail.com>
2023-06-10 03:40:00 +0000

    docs

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,9 +2,7 @@
 '''This mods purpose is to create and empty class'''
 
 
-class BasegGeometry:
-
-    '''Base class for geometry-related operations,
+class BasegGeometry: '''Base class for geometry-related operations,
     this class serves as a base for implementing
     geometry-related operations'''
     pass
